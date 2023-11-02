b7e27ea424f2755411c967b599cc94234a753ede
IvanLean <96229064+IvanLean@users.noreply.github.com>
2023-11-02 14:22:41 +0300

    Add files via upload
    Реализовать C# проект:
    4.1. DeskTop приложение должно выводить данные из БД в таблицу
    данные в GUI.
    4.2. Запрос к БД должен быть реализован с помощью хранимой
    процедуры из задания 1, пункт 5. Входные данные для хранимой процедуры
    должны задаваться с формы (текстовые поля).
    4.3. Осуществление запроса должно осуществляться по нажатию на
    кнопку на форме/ либо по клавише Enter.
    4.4. Результаты должны помещаться в таблицу на форме.

---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace indaSoftTZ
+{
+    public partial class Form1 : Form
+    {
+
+
+        string connectionString = ConfigurationManager.ConnectionStrings["indaSoft"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получение введенных данных
+            int newDepartmentId = int.Parse(textBox1.Text); 
+            int newPrecent = int.Parse(textBox2.Text);
+            // Выполнение запроса к БД с использованием хранимой процедуры
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                // Открытие подключения и выполнение запроса
+                connection.Open();
+                using (var command = new NpgsqlCommand("SELECT * FROM updatesalaryfordepartment2(@newdepartment_id, @percent)", connection))
+                {
+                    // Добавление входных параметров для процедуры
+                    command.Parameters.AddWithValue("newdepartment_id", newDepartmentId);
+                    command.Parameters.AddWithValue("percent", newPrecent); 
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+
+                        // Отображение результатов запроса в DataGridView
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+                
+            }
+        }
+    
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
